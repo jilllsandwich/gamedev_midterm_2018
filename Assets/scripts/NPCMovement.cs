e9bec661710f151addd46b68d98fc67bc7ee52b1
McKenna <msf399@nyu.edu>
2018-10-04 16:38:04 -0400

    npc move code

---
+++ Assets/scripts/NPCMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour {
+	
+	// player movement variables
+	public static int moveSpeed = 1;
+	Vector3 userDirection = Vector3.right;	
+
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate(userDirection * moveSpeed * Time.deltaTime);
+	}
+}

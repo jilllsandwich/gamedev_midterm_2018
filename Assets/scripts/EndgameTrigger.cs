f209c89dbbcb23c0d5bda8361da5c2442b599e9a
McKenna <msf399@nyu.edu>
2018-10-19 16:26:47 -0400

    wish i remembered how to stage all. anyway, here's assets

---
+++ Assets/scripts/EndgameTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndgameTrigger : MonoBehaviour
+{
+
+	void OnTriggerEnter(Collider ChangeScene) 
+	{
+		if (ChangeScene.gameObject.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(sceneName:"EndgameScreen");
+		}
+	}
+}
---
+++ Assets/scripts/NPCForward.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCForward : MonoBehaviour {
+
+	// player movement variables
+	public static int moveSpeed = 1;
+	Vector3 userDirection = Vector3.right;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(userDirection * moveSpeed * Time.deltaTime);
+	}
+}
